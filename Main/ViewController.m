a25fe632943ede00d412c131f823562193563e85
Per Ersson <per.ersson@gmail.com>
2012-09-12 20:48:00 +0200

    Renamed to Bettery

---
+++ Main/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FlopCoach
+//
+//  Created by Per Ersson on 2011-12-17.
+//  Copyright (c) 2011 itelect AB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {        
+    }
+    
+    return self;
+}
+
+-(void) dealloc {
+    [super dealloc];
+}
+
+- (void) awakeFromNib {
+}
+
+@end
--- data/buildall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/ah
-for nr in 2 3 4 5 6 7 8 9 10 ; do ../compile_data.py $nt.txt > $nr.plist ; done
